0613a29224e32542c3ec7a8f63f815020007400b
andresclemente68 <andresclemente68@gmail.com>
2024-05-16 21:28:33 -0400

    calculadora1

---
+++ script.js
@@ -0,0 +1,46 @@
+function suma(){
+    let termino1 = document.getElementById("digito1").value;
+    let termino2 = document.getElementById("digito2").value;
+    let resultado = +termino1 + +termino2;
+
+
+    alert("resultado es " + resultado)
+    
+
+}
+
+
+function resta(){
+    let termino1 = document.getElementById("digito1").value;
+    let termino2 = document.getElementById("digito2").value;
+    let resultado = +termino1 - +termino2;
+
+
+    alert("resultado es " + resultado)
+    
+
+}
+
+function multipica(){
+    let termino1 = document.getElementById("digito1").value;
+    let termino2 = document.getElementById("digito2").value;
+    let resultado = +termino1 * +termino2;
+
+
+    alert("resultado es " + resultado)
+    
+
+}
+
+function divide(){
+    let termino1 = document.getElementById("digito1").value;
+    let termino2 = document.getElementById("digito2").value;
+    let resultado = +termino1 / +termino2;
+
+
+    alert("resultado es " + resultado)
+    
+
+}
+
+
